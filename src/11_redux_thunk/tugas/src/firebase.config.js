cb7c255c43fb32c23e18c26b6863e9430c791832
ardananjungkusuma <ardananjungkusuma@gmail.com>
2021-05-24 11:46:06 +0700

    11

---
+++ src/11_redux_thunk/tugas/src/firebase.config.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+export const firebaseConfig = {
+  apiKey: "AIzaSyA_YYqVRIcKMf7IMWxiXZMlJ1VYdfT6NYY",
+  authDomain: "belajar-react-f1fa5.firebaseapp.com",
+  projectId: "belajar-react-f1fa5",
+  storageBucket: "belajar-react-f1fa5.appspot.com",
+  messagingSenderId: "623906692914",
+  appId: "1:623906692914:web:608dc95aae7eb1f6d9c12b",
+  measurementId: "G-7GJ8YWHZ3Z",
+};
+export const myFirebase = firebase.initializeApp(firebaseConfig);
