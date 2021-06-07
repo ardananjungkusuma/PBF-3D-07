2b91c4111adf353a2e03db0ae25312102715fbb3
ardananjungkusuma <ardananjungkusuma@gmail.com>
2021-06-07 15:55:52 +0700

    13

---
+++ src/13_quasar/src/pages/Index.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-page class="flex flex-center">
+    <h3>{{ hello }}</h3>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: "PageIndex",
+  data() {
+    return {
+      hello: "Halo saya Ardan Anjung - NIM 1841720041"
+    };
+  }
+};
+</script>
